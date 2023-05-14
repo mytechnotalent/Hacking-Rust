1a33016b846f172639ac31e4d2f9150be0a61849
Kevin Thomas <kevin@mytechnotalent.com>
2023-05-14 13:09:29 -0400

    Added files for Chapter 4

---
+++ two_scalar-data-types/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let my_integer: i32 = 42;
+    println!("integer: {}", my_integer);
+
+    let my_float: f64 = 3.14;
+    println!("float: {}", my_float);
+
+    let my_char: char = 'A';
+    println!("character: {}", my_char);
+}
